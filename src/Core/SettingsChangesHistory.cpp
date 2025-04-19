3e43742116504bf838fd1f03f9a340b9480cf656
Kermit <chenjiawei1@xiaohongshu.com>
2025-04-19 08:21:43 +0200

    fix doc & add settings changes history

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -73,6 +73,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"implicit_table_at_top_level", "", "", "A new setting, used in clickhouse-local"},
             {"use_skip_indexes_if_final_exact_mode", 0, 0, "This setting was introduced to help FINAL query return correct results with skip indexes"},
             {"parallel_replicas_insert_select_local_pipeline", false, false, "Use local pipeline during distributed INSERT SELECT with parallel replicas. Currently disabled due to performance issues"},
+            {"skip_unavailable_shards_mode", "unavailable_or_exception", "unavailable", "If set to `unavailable`, connection-related exceptions are ignored, whereas `unavailable_or_exception` ignores all exceptions from remote instances."},
         });
         addSettingsChanges(settings_changes_history, "25.4",
         {
